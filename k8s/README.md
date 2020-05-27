055a7e37c9aa03c9dfde94c9cdc46496992c033a
Adrian Hy <ahy@MacBook-Pro.local>
2020-05-27 13:59:24 -0700

    cleaned up repository for k8s environment

---
+++ k8s/README.md
@@ -0,0 +1,1 @@
+This directory is going to house all the configuration files for our K8s cluster.
--- react-client/src/Fib.js
+++ react-client/src/Fib.js
@@ -27,7 +27,7 @@ class Fib extends Component {
 
     renderSeenIndexes() {
         return this.state.seenIndexes.map(({ number }) => number).join(', ');
-      }
+    }
     
     renderValues() {
         const entries = [];
